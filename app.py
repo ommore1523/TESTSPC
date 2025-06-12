ac0ff4b2c54314bba0662706915bee24be10ccd6
omkar <omkar@lasso.com>
2025-06-12 13:41:04 +0530

    test

---
+++ app.py
@@ -0,0 +1,16 @@
+print("hello")
+
+"""
+
+git init --> git initialize in folder 
+
+git status --> show file status 
+
+git branch -M master
+
+1. untracked files :  the files which you not pushed into repository yet
+2. Changes to be committed  : file us pushed to  commit area  
+
+git remote add origin <repositoy url>
+
+"""
